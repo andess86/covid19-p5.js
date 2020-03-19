d509b238e02f45949c1983bf36810999ecddc0a9
Anders Svensson <andess86@gmail.com>
2020-03-19 12:29:08 +0100

    Add text, trigger noLoop when done

--- sketch.js
+++ sketch.js
@@ -1,10 +1,6 @@
 var mapimg;
 var clat = 0;
 var clon = 0;
-
-var lat = 31.2304;
-var long = 121.4737;
-
 var zoom = 1;
 var deaths;
 var confirmed;
@@ -12,6 +8,7 @@ var recovered;
 var fr = 15;
 var iterator = 6;
 
+var totalDeathsToday = 0;
 function preload() {
   mapimg = loadImage(
     `https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,${zoom},0,0/1024x512?access_token=pk.eyJ1IjoiYW5kZXNzODYiLCJhIjoiY2p3dzJ2NXozMW1xcTN6cDU1cmN5OTVkMSJ9.QusmuAsR11ZNzLtt0m-CsA`
@@ -47,25 +44,35 @@ function draw() {
   translate(width / 2, height / 2);
   imageMode(CENTER);
   image(mapimg, 0, 0);
+  textSize(32);
+  fill(255, 0, 10, 150);
+  text('Date:' + ' ' + dataLength[iterator], -400, 250);
+  // Framerate is 15fps, therefore devide by 15.
+  text('Dead:' + ' ' + Math.ceil(totalDeathsToday / 15), -100, 250);
+  if (iterator < dataLength.length - 1) {
+    iterator++;
+  } else {
+    noLoop();
+  }
+  drawDeath();
+}
 
+function drawDeath() {
   var centerX = mercX(clon);
   var centerY = mercY(clat);
-
   // Start at index 1 to skip headers
   for (var i = 1; i < deaths.length; i++) {
     var data = deaths[i].split(/,/);
     var lat = data[2];
     var long = data[3];
     var dead = data[iterator];
+    // console.log(totalDeathsToday);
     var x = mercX(long) - centerX;
     var y = mercY(lat) - centerY;
     fill(255, 0, 10, 50);
     ellipse(x, y, Number(dead) / 10, Number(dead) / 10);
+    totalDeathsToday = totalDeathsToday + Number(dead);
   }
-  if (iterator < dataLength.length - 1) {
-    iterator++;
-  }
-  console.log(iterator);
 }
 
 function setup() {
